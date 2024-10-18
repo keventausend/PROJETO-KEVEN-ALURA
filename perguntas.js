0d8ed8a2de362e10e4b7a71ce34c2686240a0478
keventausend <165857946+keventausend@users.noreply.github.com>
2024-10-17 21:54:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Animais',
+    'Qual é o animal mais rápido do mundo?',
+    'O falcão-peregrino'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o planeta mais quente do sistema solar?',
+    'Vênus é o planeta mais quente, com temperaturas que podem chegar a 465°C devido à sua densa atmosfera de dióxido de carbono.'
+)
+
+criaCartao(
+    'Medicina',
+    'Quantos ossos tem o corpo humano adulto?',
+    'Um corpo humano adulto possui 206 ossos, embora os recém-nascidos tenham cerca de 270, alguns dos quais se fundem durante o crescimento.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que é o efeito Mandela?',
+    'O efeito Mandela é um fenômeno em que um grande grupo de pessoas lembra de um evento ou fato de forma diferente da realidade, como a crença de que Nelson Mandela morreu na prisão na década de 1980.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Qual é a flor nacional do Japão?',
+    'A flor nacional do Japão é a sakura, ou flor de cerejeira, que simboliza a beleza e a transitoriedade da vida.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é inteligência artificial (IA)?',
+    ' Inteligência artificial (IA) é a simulação de processos de inteligência humana por máquinas, especialmente sistemas computacionais, que incluem aprendizado, raciocínio e autocorreção.'
+)
